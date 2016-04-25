c9d773316090ada79f170e83e97992cf9b9d9ba8
Michael van Tricht <swordbeta@users.noreply.github.com>
2016-04-25 17:38:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# redis-http-push-queue
+Listens to a specific redis queue and posts the payloady body to the payload endpoint.
---
+++ src/index.js
@@ -0,0 +1,32 @@
+'use strict';
+import request from 'request';
+import mkdebug from 'debug';
+import config from 'config';
+import redis from 'redis';
+
+const debug = mkdebug('redis-http-push-queue:log');
+const error = mkdebug('redis-http-push-queue:error');
+
+const connect = (host, port) => redis.createClient(`redis://${host}:${port}`);
+
+export default () => {
+  debug('Opening redis connection');
+  const client = connect(config.get('redis.host'), config.get('redis.port'));
+  client.on('message', (channel, message) => {
+    message = JSON.parse(message);
+    request.post({
+      json: true,
+      url: message.endpoint,
+      body: message.args
+    }, (err, res, body) => {
+      if (err) {
+        error(err);
+        return;
+      }
+      const resMsg = `Got back statuscode ${res.statusCode} with body ${body}`;
+      res.statusCode >= 200 && res.statusCode <= 299 ? debug(resMsg) : error(resMsg);
+    });
+  });
+  debug(`Subscribing to ${config.get('redis.queue')}`);
+  client.subscribe(config.get('redis.queue'));
+};
