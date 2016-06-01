65699b293940ac83b255f3377b493bd231faf056
Ron <ron_oudgenoeg@hotmail.com>
2016-06-01 14:48:03 +0200

    S76.10 Make the queue synchronous and adds the ability to stop retrying after x attempts

--- src/index.js
+++ src/index.js
@@ -1,41 +1,19 @@
 'use strict';
-import request from 'request';
 import mkdebug from 'debug';
 import config from 'config';
 import redis from 'redis';
-import {any, equals} from 'ramda';
+import {run, add} from './stack';
 
 const debug = mkdebug('redis-http-push-queue:log');
-const error = mkdebug('redis-http-push-queue:error');
 
 const connect = (host, port) => redis.createClient(`redis://${host}:${port}`);
 
 export default () => {
   debug('Opening redis connection');
   const client = connect(config.get('redis.host'), config.get('redis.port'));
-  const publishClient = connect(config.get('redis.host'), config.get('redis.port'));
+  run(0);
   client.on('message', (channel, message) => {
-    const parsedMessage = JSON.parse(message);
-    request.post({
-      json: true,
-      url: parsedMessage.endpoint,
-      body: parsedMessage.args,
-      headers: config.get('headers')
-    }, (err, res, body) => {
-      if (err) {
-        error(err);
-        return;
-      }
-      const resMsg = `Got back statuscode ${res.statusCode} with body ${JSON.stringify(body)}`;
-      if (res.statusCode >= 200 && res.statusCode <= 299) {
-        debug(resMsg);
-      } else {
-        error(resMsg);
-      }
-      if (any(equals(res.statusCode), config.get('redis.retryCodes'))) {
-        setTimeout(() => publishClient.publish(channel, message), 3000);
-      }
-    });
+    add(JSON.parse(message));
   });
   debug(`Subscribing to ${config.get('redis.queue')}`);
   client.subscribe(config.get('redis.queue'));
